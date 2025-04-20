06170b340b7376f70e9a12a28c3155e95fc45573
hamletrpg <hamletgil08@gmail.com>
2025-04-19 23:00:08 -0400

    added more vectors functionalities, need a bit more digging on angles, radians to deg business

--- vectors.cpp
+++ vectors.cpp
@@ -27,7 +27,6 @@ vec3 operator-(const vec3& l, const vec3& r)
 {
     return {l.x - r.x, l.y - r.y, l.z - r.z};
 }
-
 // Multiplication operator overloading
 vec2 operator*(const vec2& l, const vec2& r)
 {
@@ -76,4 +75,65 @@ float Dot(const vec2& l, const vec2& r)
 float Dot(const vec3& l, const vec3& r)
 {
     return l.x * r.x + l.y * r.y + l.z * r.z;
+}
+
+// Magnitude
+float Magnitude(const vec2& v)
+{
+    return sqrtf(Dot(v, v));
+}
+
+float Magnitude(const vec3& v)
+{
+    return sqrtf(Dot(v, v));
+}
+
+float MagnitudeSq(const vec2& v)
+{
+    return Dot(v, v);
+}
+float MagnitudeSq(const vec3& v)
+{
+    return Dot(v, v);
+}
+
+// Normalized Vectors
+void Normalize(vec2& v)
+{
+    v = v *(1.0f / Magnitude(v));
+}
+void Normalize(vec3& v)
+{
+    v = v * (1.0f / Magnitude(v));
+}
+
+vec2 Normalized(const vec2& v)
+{
+    return v * (1.0f / Magnitude(v));
+}
+vec3 Normalized(const vec3& v)
+{
+    return v * (1.0f / Magnitude(v));
+}
+
+// Cross Product
+vec3 Cross(const vec3& l, const vec3& r)
+{
+    vec3 result;
+    result.x = l.y * r.z - l.z * r.y;
+    result.y = l.z * r.x - l.x * r.z;
+    result.z = l.x * r.y - l.y * r.x;
+    return result;
+}
+
+// Angle
+float Angle(const vec2& l, const vec2& r)
+{
+    float m = sqrt(MagnitudeSq(l) * Magnitude(r));
+    return acos(Dot(l, r) / m);
+}
+float Angle(const vec3& l, const vec3& r)
+{
+    float m = sqrtf(MagnitudeSq(l) * MagnitudeSq(r));
+    return acos(Dot(l, r) / m);
 }
--- vectors.h
+++ vectors.h
@@ -1,6 +1,12 @@
 #ifndef _H_MATH_VECTORS_
 #define _H_MATH_VECTORS
 
+// the C standard librady acos return values in radians rather than degree
+// It's more intuitive to work with degrees when working with angles
+// so these macros will come handy
+#define RAD2DEG(x) ((x) * 57.29574f)
+#define DEG2RAD(x) ((x) * 0.0174533f)
+
 typedef struct vec2 
 {
     union
@@ -49,6 +55,25 @@ bool operator==(const vec3& l, const vec3& r);
 bool operator!=(const vec2& l, const vec2& r);
 bool operator!=(const vec3& l, const vec3& r);
 float Dot(const vec2& l, const vec2& r);
-float Dot(const vec2& l, const vec3& r);
+float Dot(const vec3& l, const vec3& r);
+// Some magic is being done to avoid expensive operations regarding checking for square root
+// Rather we apply the dot product with the vector we want to get the magnitude with itself
+// Normally we use the magnitude/lenght of a vector to compare it with known numbers (make sense)
+// Taking square root for comparation is expensive for such a "trivial task"
+// ex:
+// if (Magnitude(someVector) < 5.0) { do or don't do something }
+// we'd rather do
+// if (MagnitudeSq(someVector)) < 5.0f * 5.0f {do the thing}
+float Magnitude(const vec2& v);
+float Magnitude(const vec3& v);
+float MagnitudeSq(const vec2& v);
+float MagnitudeSq(const vec3& v);
+void Normalize(vec2& v);
+void Normalize(vec3& v);
+vec2 Normalized(const vec2& v);
+vec3 Normalized(const vec3& v);
+vec3 Cross(const vec3& l, const vec3& r);
+float Angle(const vec2& l, const vec2& r);
+float Angle(const vec3& l, const vec3& r);
 
 #endif
