2ba5a702cf4cce1e7ea99d6a7f9394897b498d83
hamletrpg <hamletgil08@gmail.com>
2025-10-26 18:17:18 -0400

    started geometry section

---
+++ geometry2d.h
@@ -0,0 +1,46 @@
+#ifndef _H_2D_GEOMETRY_
+#define _H_2D_GEOMETRY_
+
+#include "vectors.h"
+
+typedef vec2 Point2D;
+
+typedef struct Line2D
+{
+    Point2D start;
+    Point2D end;
+
+    inline Line2D() {}
+    inline Line2D(const Point2D& s, const Point2D& e): start(s), end(e) { } 
+} Line2D;
+
+typedef struct Circle
+{
+    Point2D position;
+    float radius;
+    
+    inline Circle() : radius(1.0f) {}
+
+    inline Circle(const Point2D& p, float r) : position(p), radius(r) { }
+} Circle;
+
+typedef struct Rectangle2D
+{
+    Point2D origin;
+    vec2 size;
+
+    inline Rectangle2D() : size(1, 1) { }
+    inline Rectangle2D(const Point2D& o, const vec2& s) : origin(o), size(s) { }
+} Rectangle2D;
+
+Rectangle2D FromMinMax(const vec2& min, const vec2& max)
+{
+    return Rectangle2D(min, max - min);
+}
+
+float Length(const Line2D& line);
+float LengthSq(const Line2D& line);
+vec2 GetMin(const Rectangle2D& rect);
+vec2 GetMax(const Rectangle2D& rect);
+
+#endif
